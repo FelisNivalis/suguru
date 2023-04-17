25564213c59a1a16055098a48523d0792cc69928
FelisNivalis <FelisNivalis@gmail.com>
2023-04-17 12:29:40 +0200

    improved search performance with naked/hidden singles

--- libs/SudokuLib/SudokuBase.cs
+++ libs/SudokuLib/SudokuBase.cs
@@ -1,4 +1,7 @@
 using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Xml;
 
 namespace SudokuLib
 {
@@ -10,7 +13,7 @@ namespace SudokuLib
         public bool[,,] candidates = new bool[9, 9, 10];
         protected int seed = -1;
         protected Random rand;
-        private const int MAX_TRIES = 1000000;
+        private const int MAX_TRIES = 10000;
 
         public SudokuBase(SudokuBase other)
         {
@@ -30,9 +33,11 @@ namespace SudokuLib
 
         public void Generate()
         {
+            var startTime = DateTime.Now;
             Clear();
             int[,]? _answer;
-            while (!Solve(out _answer, ref board, out _));
+            //while (!Solve(out _answer, ref board, out _
+            while (!Solve2(1, ref board, out _answer)) ;
             if (_answer == null) return;
             answer = _answer;
             board = answer.Clone() as int[,];
@@ -49,7 +54,8 @@ namespace SudokuLib
                         continue;
                     board[x, y] = 0;
                     bool unique;
-                    if (Solve(out _, ref board, out unique) && unique)
+                    //if (Solve(out _, ref board, out unique) && unique)
+                    if (!Solve2(2, ref board, out _))
                     {
                         flag = true;
                         break;
@@ -60,6 +66,7 @@ namespace SudokuLib
                 if (!flag) break;
             }
             init_board = board.Clone() as int[,];
+            Debug.Print(String.Format("{0}", (DateTime.Now - startTime).TotalMilliseconds));
         }
 
         protected void Clear()
@@ -85,115 +92,251 @@ namespace SudokuLib
                     clues[1, j, candidate]++;
                     clues[2, idx.Item1, candidate]++;
                 }
-            /*for (int i = 0; i < 9; i++)
-            {
-                String s = "";
-                for (int j = 0; j < 9; j++)
-                {
-                    s += board[i, j].ToString();
-                }
-                Debug.Print(s);
-            }*/
-            answer = board.Clone() as int[,];
-            int level = 0;
-            while (level < 9 * 9 && board[level / 9, level % 9] > 0) level++;
-            int nSolution = 0;
-            LinkedList<int[]> candidatesList = new();
-            candidatesList.AddLast(Enumerable.Range(0, 10).ToArray());
-            while (nTries++ < MAX_TRIES)
-            {
-                int r = level / 9;
-                int c = level % 9;
-                var idx = Common.GetIdxFromRC(r, c);
-                var candidates = candidatesList.Last();
-                if (candidates[0] > 0)
-                {
-                    int candidate = candidates[candidates[0]];
-                    clues[0, r, candidate]--;
-                    clues[1, c, candidate]--;
-                    clues[2, idx.Item1, candidate]--;
-                }
-                while (candidates[0]++ < 9)
-                {
-                    int cur = candidates[0];
-                    int rIdx = rand.Next(candidates[0], 10);
-                    int candidate = candidates[rIdx];
-                    candidates[rIdx] = candidates[cur];
-                    candidates[cur] = candidate;
-                    if (clues[0, r, candidate] == 0 && clues[1, c, candidate] == 0 && clues[2, idx.Item1, candidate] == 0)
-                        break;
-                }
-                
-                if (candidates[0] <= 9)
-                {
-                    int candidate = candidates[candidates[0]];
-                    answer[r, c] = candidate;
-                    clues[0, r, candidate]++;
-                    clues[1, c, candidate]++;
-                    clues[2, idx.Item1, candidate]++;
-                    //Debug.Print(String.Format("Search level={0}, candidateIdx={1}", level, candidates[0]));
-                    while (++level < 9 * 9 && board[level / 9, level % 9] > 0) ;
-                    candidatesList.AddLast(Enumerable.Range(0, 10).ToArray());
-                    if (level == 9 * 9)
-                    {
-                        nSolution++;
-                        if (nSolution > 1)
-                            break;
-                    }
-                }
-                if (candidates[0] > 9 || level == 9 * 9)
-                {
-                    //Debug.Print(String.Format("Backtracking level={0}", level));
-                    candidatesList.RemoveLast();
-                    while (--level >= 0 && board[level / 9, level % 9] > 0) ;
-                    if (level < 0) break;
-                }
-            }
-            unique = nSolution == 1;
-            bool ret = nSolution > 0;
-            //bool ret = _Solve(out answer, ref board, out unique, 0, ref nTries, ref clues);
-            Debug.Print(String.Format("{0}:{1}:solved={2}:unique={3}", (DateTime.Now - startTime).TotalSeconds, nTries, ret, unique));
+            double timeCount = 0;
+            bool ret = _Solve(out answer, ref board, out unique, ref nTries, ref clues, ref timeCount);
+            if (nTries > 1000) Debug.Print(String.Format("{0}:sortTime={4}:{1}:solved={2}:unique={3}", (DateTime.Now - startTime).TotalNanoseconds / nTries, nTries, ret, unique, timeCount / nTries));
             return ret;
         }
 
-        protected bool _Solve(out int[,]? answer, ref int[,] board, out bool unique, int level, ref int nTries, ref int[,,] clues)
+        struct StackInfo
+        {
+            public int idx = 0;
+            public int digit = 0;
+            public int[,] board = new int[9, 9];
+            public int[,] candidates = new int[9, 9];
+            public int[,] nCandidatesRow = new int[9, 10];
+            public int[,] nCandidatesCol = new int[9, 10];
+            public int[,] nCandidatesGrid = new int[10, 10];
+            public StackInfo(in int[,] board)
+            {
+                // New stack. Find the first empty subgrid.
+                this.board = board.Clone() as int[,];
+                Next();
+                // Init candidates
+                Queue<(int, int, int)> q = new();
+                for (int i = 0; i < 9; i++)
+                    for (int j = 0; j < 9; j++)
+                        if (board[i, j] > 0)
+                            q.Enqueue((i, j, board[i, j]));
+                Fill(ref q);
+            }
+            public void Next()
+            {
+                for (; idx < 9 * 9 && board[idx / 9, idx % 9] != 0; idx++) ;
+                digit = 0;
+            }
+            public StackInfo(StackInfo s)
+            {
+                idx = s.idx;
+                digit = s.digit;
+                board = s.board.Clone() as int[,];
+                candidates = s.candidates.Clone() as int[,];
+                nCandidatesRow = s.nCandidatesRow.Clone() as int[,];
+                nCandidatesCol = s.nCandidatesCol.Clone() as int[,];
+                nCandidatesGrid = s.nCandidatesGrid.Clone() as int[,];
+            }
+            static public int[] cand = new int[1 << 9];
+            static StackInfo()
+            {
+                Array.Clear(cand, 0, cand.Length);
+                for (int i = 0; i < 9; i++)
+                    cand[(1 << 9) - 1 - (1 << i)] = i + 1;
+            }
+            public record struct UpdateCandidatesType(bool CheckRow, bool CheckCol, bool CheckGrid) { }
+            public bool UpdateCandidates(ref Queue<(int, int, int)> q, int r, int c, int d, UpdateCandidatesType type)
+            {
+                // Update after crossing out a candidate
+                int b = 1 << d - 1;
+                if (board[r, c] == 0 && (candidates[r, c] & b) == 0)
+                {
+                    candidates[r, c] |= b;
+                    // Check naked single
+                    int cand = StackInfo.cand[candidates[r, c]];
+                    if (cand > 0) q.Enqueue((r, c, cand));
+                    if (candidates[r, c] == (1 << 9) - 1) return false;
+                    nCandidatesRow[r, d]++;
+                    // nCandidatesRow
+                    if (type.CheckRow)
+                    {
+                        // Hidden single
+                        if (nCandidatesRow[r, d] == 8)
+                            for (int j = 0; j < 9; j++)
+                                if ((candidates[r, j] & b) == 0)
+                                    q.Enqueue((r, j, d));
+                        if (nCandidatesRow[r, d] == 9) return false;
+                    }
+                    // nCandidatesCol
+                    nCandidatesCol[c, d]++;
+                    if (type.CheckCol)
+                    {
+                        // Hidden single
+                        if (nCandidatesCol[c, d] == 8)
+                            for (int j = 0; j < 9; j++)
+                                if ((candidates[j, c] & b) == 0)
+                                    q.Enqueue((j, c, d));
+                        if (nCandidatesCol[c, d] == 9) return false;
+                    }
+                    
+                    var (idx_grid, idx_subgrid) = Common.GetIdxFromRC(r, c);
+                    nCandidatesGrid[idx_grid, d]++;
+                    if (type.CheckGrid)
+                    {
+                        // Hidden single
+                        if (nCandidatesGrid[idx_grid, d] == 8)
+                            for (int j = 0; j < 9; j++)
+                            {
+                                var (_r, _c) = Common.GetRCFromIdx(idx_grid, j + 1);
+                                if ((candidates[_r, _c] & b) == 0)
+                                    q.Enqueue((_r, _c, d));
+                            }
+                        if (nCandidatesGrid[idx_grid, d] == 9) return false;
+                    }
+                }
+                return true;
+            }
+            public bool Fill(ref Queue<(int, int, int)> q)
+            {
+                while (q.Count > 0)
+                {
+                    var (r, c, d) = q.Dequeue();
+                    if (!_Fill(ref q, r, c, d)) return false;
+                }
+                return true;
+            }
+            public bool _Fill(ref Queue<(int, int, int)> q, int r, int c, int d)
+            {
+                board[r, c] = d;
+                var idx = Common.GetIdxFromRC(r, c);
+                for (int i = 0; i < 9; i++)
+                {
+                    // Don't check row when crossing out same row, similar for col
+                    // For grid, only when crossing out the last one of the grid, and not in the same grid as (_r, _c)
+                    if (!UpdateCandidates(ref q, r, i, d, new UpdateCandidatesType(false, true, i % 3 == 2 && i / 3 != c / 3))) return false;
+                    if (!UpdateCandidates(ref q, i, c, d, new UpdateCandidatesType(true, false, i % 3 == 2 && i / 3 != r / 3))) return false;
+                    var (_r, _c) = Common.GetRCFromIdx(idx.Item1, i + 1);
+                    if (!UpdateCandidates(ref q, _r, _c, d, new UpdateCandidatesType(_r != r && _c % 3 == 2, _c != c && _r % 3 == 2, false))) return false;
+                }
+                return true;
+            }
+        }
+
+        protected bool Solve2(in int nSolutionsToFind, ref int[,] board, out int[,]? answer)
+        {
+            answer = null;
+            int nSolution = 0;
+            Stack<StackInfo> stack = new ();
+            stack.Push(new StackInfo(board));
+            while (stack.Count > 0)
+            {
+                var s = stack.Pop();
+                if (s.idx == 9 * 9)
+                {
+                    if (nSolution == 0)
+                        answer = s.board.Clone() as int[,];
+                    nSolution++;
+                    if (nSolution >= nSolutionsToFind)
+                        break;
+                    continue;
+                }
+                int r = s.idx / 9;
+                int c = s.idx % 9;
+                // Try every candidate
+                for (s.digit++; s.digit <= 9; s.digit++)
+                {
+                    int b = 1 << s.digit - 1;
+                    if ((s.candidates[r, c] & b) == 0)
+                    {
+                        // Until no hidden singles and naked singles
+                        Queue<(int, int, int)> q = new();
+                        q.Enqueue((r, c, s.digit));
+                        var ns = new StackInfo(s);
+                        if (ns.Fill(ref q))
+                        {
+                            ns.Next();
+                            stack.Push(s);
+                            stack.Push(ns);
+                            break;
+                        }
+                    }
+                }
+            }
+            return nSolution >= nSolutionsToFind;
+        }
+
+        protected bool _Solve(out int[,]? answer, ref int[,] board, out bool unique, ref int nTries, ref int[,,] clues, ref double timeCount)
         {
             answer = null;
             unique = true;
             // Get current subgrid.
-            if (level == 81)
+            int r, c;
+            r = c = 0;
+            (int, int) idx;
+            int[] order = Enumerable.Range(1, 9).ToArray();
+            Shuffle(ref order);
+            // Find a subgrid with least possible clues
+            int minNV = 10;
+            var startTime = DateTime.Now;
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++)
+                    if (board[i, j] == 0)
+                    {
+                        int nV = 0;
+                        idx = Common.GetIdxFromRC(i, j);
+                        int cand = 0;
+                        for (int d = 1; d <= 9; d++)
+                            if (clues[0, i, d] == 0 && clues[1, j, d] == 0 && clues[2, idx.Item1, d] == 0)
+                            {
+                                nV++;
+                                cand = d;
+                            }
+                        // No solutions
+                        if (nV == 0)
+                        {
+                            timeCount += (DateTime.Now - startTime).TotalNanoseconds;
+                            return false;
+                        }
+                        if (nV < minNV)
+                        {
+                            minNV = nV;
+                            r = i;
+                            c = j;
+                            if (nV == 1)
+                            {
+                                order = new[] { cand };
+                                goto L1;
+                            }
+                        }
+                    }
+            L1:
+            timeCount += (DateTime.Now - startTime).TotalNanoseconds;
+            if (minNV == 10)
             {
                 answer = board.Clone() as int[,];
                 return true;
             }
-            int i = level / 9;
-            int j = level % 9;
-            if (board[i, j] > 0) return _Solve(out answer, ref board, out unique, level + 1, ref nTries, ref clues);
-            var idx = Common.GetIdxFromRC(i, j);
+            idx = Common.GetIdxFromRC(r, c);
             // Search
             int nSolution = 0;
-            int[] order = Enumerable.Range(1, 9).ToArray();
-            Shuffle(ref order);
             foreach (int candidate in order)
             {
-                if (clues[0, i, candidate] > 0 || clues[1, j, candidate] > 0 || clues[2, idx.Item1, candidate] > 0)
+                if (clues[0, r, candidate] > 0 || clues[1, c, candidate] > 0 || clues[2, idx.Item1, candidate] > 0)
                     continue;
-                board[i, j] = candidate;
-                clues[0, i, candidate]++;
-                clues[1, j, candidate]++;
+                board[r, c] = candidate;
+                clues[0, r, candidate]++;
+                clues[1, c, candidate]++;
                 clues[2, idx.Item1, candidate]++;
                 bool _unique;
                 int[,]? _answer;
-                if (++nTries < MAX_TRIES && _Solve(out _answer, ref board, out _unique, level + 1, ref nTries, ref clues))
+                if (++nTries < MAX_TRIES && _Solve(out _answer, ref board, out _unique, ref nTries, ref clues, ref timeCount))
                 {
                     answer = _answer;
                     nSolution++;
                     if (!_unique)
                         nSolution++;
                 }
-                board[i, j] = 0;
-                clues[0, i, candidate]--;
-                clues[1, j, candidate]--;
+                board[r, c] = 0;
+                clues[0, r, candidate]--;
+                clues[1, c, candidate]--;
                 clues[2, idx.Item1, candidate]--;
                 if (nTries >= MAX_TRIES) return false;
                 if (nSolution > 1) break;
